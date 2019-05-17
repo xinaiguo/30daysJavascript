6e2f6d22696ef32a3978dda11cfb2318fe932352
Xinai <xinai.guo@siemens.com>
2019-05-17 15:23:46 +0800

    add comments

--- 05 - Flex Panel Gallery/animation.js
+++ 05 - Flex Panel Gallery/animation.js
@@ -1,7 +1,7 @@
 const panels = Array.from(document.querySelectorAll('.panel'));
 
 function toggleOpen(){
-    //当只有一个参数时：切换 class value; 即如果类存在，则删除它并返回false，如果不存在，则添加它并返回true。
+    //在元素中切换类名。第一个参数为要在元素中移除的类名，并返回false，如果该类名不存在则在元素中添加类名，并返回true
     this.classList.toggle('open');
 }
 
